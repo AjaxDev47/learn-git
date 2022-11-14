0c0428991f506a5a9edb2c26490b4109fbdd734b
AjaxDev47 <msiajax@gmail.com>
2022-11-14 23:33:11 +0530

    Message Difference Added

--- Diff.java
+++ Diff.java
@@ -2,7 +2,7 @@ public class Diff {
 
     public static void main(String[] args){
         int a = 6, b = 4;
-        System.out.println(a-b);
+        System.out.println("Difference: "+(a-b));
     }
     
 }
